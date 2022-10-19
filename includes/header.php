17597bed8b0111f3d382e17f7bb6cb53c1433275
MrSuPilun <phamngocan01627@gmail.com>
2022-10-19 19:52:00 +0700

    PHP004 fix footer and js

--- includes/header.php
+++ includes/header.php
@@ -2,13 +2,13 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<title><?php echo $page_title; ?></title>	
-	<link rel="stylesheet" href="style.css" type="text/css" />
+	<link rel="stylesheet" href="includes/style.css" type="text/css" />
 	<link rel="stylesheet" href="assets/vendor/bootstrap-4.6.2-dist/css/bootstrap.min.css" type="text/css" media="screen" />
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container">
+  <!-- <div class="container"> -->
 	  <a class="navbar-brand" href="#">Nhóm 7</a>
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	    <span class="navbar-toggler-icon"></span>
@@ -27,7 +27,7 @@
 	      </li>
 	    </ul>
 	  </div>
-  </div>
+  <!-- </div> -->
 </nav>
 <div id="content"><!-- Start of the page-specific content. -->
 <!-- Script 9.1 - header.html -->
