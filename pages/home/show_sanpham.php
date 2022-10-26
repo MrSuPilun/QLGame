6841aaffc4aa8258416de43c6fbe08f5a3063408
MrSuPilun <phamngocan01627@gmail.com>
2022-10-26 13:22:31 +0700

    PHP013 Hien Thi San Pham

--- pages/home/show_sanpham.php
+++ pages/home/show_sanpham.php
@@ -1,35 +1,49 @@
 <?php
-  // $result = $qlgame->queryDB("SELECT * FROM GAME");
 
-  // while($rows = mysqli_fetch_array($result)){
-  //   print_r($rows);
-  //   echo "<br>";
-  // }
-?>
-<style>
-  .col > img {
-    width: 100%;
-    min-height: 150px;
-    object-fit: cover;
-    border-radius: 15px;
+$str_card_sp = "";
+$result = $qlgame->queryDB("SELECT * FROM GAME");
+$countRows = mysqli_num_rows($result);
+$numCardsInRow = 5;
+$numRows = floor($countRows / $numCardsInRow) + 1;
+
+function formatMoney($number, $fractional=false) {
+  if ($fractional) {
+      $number = sprintf('%.2f', $number);
   }
-</style>
-<div class="row">
-  <div class="col">
-    <img src="https://images8.alphacoders.com/958/958091.jpg" alt="">
-  </div>
-  <div class="col">
-    <img src="https://images8.alphacoders.com/958/958091.jpg" alt="">
-  </div>
-  <div class="col">
-    <img src="https://images8.alphacoders.com/958/958091.jpg" alt="">
-  </div>
-  <div class="col">
-    <img src="https://images8.alphacoders.com/958/958091.jpg" alt="">
-  </div>
-  <div class="col">
-    <img src="https://images8.alphacoders.com/958/958091.jpg" alt="">
-  </div>
-</div>
+  while (true) {
+      $replaced = preg_replace('/(-?\d+)(\d\d\d)/', '$1,$2', $number);
+      if ($replaced != $number) {
+          $number = $replaced;
+      } else {
+          break;
+      }
+  }
+  return $number;
+}
 
+for ($i = 0; $i < $numRows; $i++) {
+  $j = 1;
+  $str_card_sp .= "<div class='list-sp row'>";
+  while ($rows = mysqli_fetch_array($result)) {
+    $str_card_sp .= "
+    <div class='card-sp col'>
+      <div class='card-thumbnail'>
+        <img src='$rows[4]'>
+        <div class='card-btn-add'>
+          <a href='#'><div class='card-icon-add'></div></a>
+        </div>
+      </div>
+      <p>$rows[1]</p>
+      <p><b><u>đ</u>" . formatMoney($rows[3]) . "</b></p>
+    </div>
+    ";
+    if ($j == 5)
+      break;
+    $j++;
+  }
+  $str_card_sp .= "</div>";
+}
 
+echo $str_card_sp;
+
+?>
